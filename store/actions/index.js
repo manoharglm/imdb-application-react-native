f4e8afe4b2ac1320f5dbdd1cb276c7d4ea8b309f
manoharglm <manohargunduboina@gmail.com>
2022-01-05 22:36:46 +0530

    Add api keys to .env file

--- store/actions/index.js
+++ store/actions/index.js
@@ -1,54 +1,6 @@
-const apikey = "3ebe7f44a343fc7c8c085ccfef4cc493"
-const token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzZWJlN2Y0NGEzNDNmYzdjOGMwODVjY2ZlZjRjYzQ5MyIsInN1YiI6IjVjNWU3MmI0YzNhMzY4M2NjZjhkNDM1MCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.XYra3lypk70Rh69HEVdi76crIKkKJZE6KY350cEB3Ow"
-// const userWatchList = [{
-//   "adult": false,
-//   "backdrop_path": "/pmLEmMLYCreFAxfdCt5uO1wDgSi.jpg",
-//   "genre_ids": [12, 16, 10751, 35],
-//   "id": 9502,
-//   "original_language": "en",
-//   "original_title": "Kung Fu Panda",
-//   "overview": "When the Valley of Peace is threatened, lazy Po the panda discovers his destiny as the \"chosen one\" and trains to become a kung fu hero, but transforming the unsleek slacker into a brave warrior won't be easy. It's up to Master Shifu and the Furious Five -- Tigress, Crane, Mantis, Viper and Monkey -- to give it a try.",
-//   "poster_path": "/2Paj1nufT0jeSY0G4u3RC31HIGT.jpg",
-//   "release_date": "2008-06-04",
-//   "title": "Kung Fu Panda",
-//   "video": false,
-//   "vote_average": 7.0,
-//   "vote_count": 5600,
-//   "popularity": 21.989,
-//   "media_type": "movie"
-// }, {
-//   "adult": false,
-//   "backdrop_path": "/eyiE1vuizMKVu8MoZkg2xsramFl.jpg",
-//   "genre_ids": [16, 10751],
-//   "id": 49444,
-//   "original_language": "en",
-//   "original_title": "Kung Fu Panda 2",
-//   "overview": "Po is now living his dream as The Dragon Warrior, protecting the Valley of Peace alongside his friends and fellow kung fu masters, The Furious Five - Tigress, Crane, Mantis, Viper and Monkey. But Po’s new life of awesomeness is threatened by the emergence of a formidable villain, who plans to use a secret, unstoppable weapon to conquer China and destroy kung fu. It is up to Po and The Furious Five to journey across China to face this threat and vanquish it. But how can Po stop a weapon that can stop kung fu? He must look to his past and uncover the secrets of his mysterious origins; only then will he be able to unlock the strength he needs to succeed.",
-//   "poster_path": "/Fzyy2dauoKxjUzUPYYo3kJkJAW.jpg",
-//   "release_date": "2011-05-25",
-//   "title": "Kung Fu Panda 2",
-//   "video": false,
-//   "vote_average": 6.7,
-//   "vote_count": 3315,
-//   "popularity": 16.654,
-//   "media_type": "movie"
-// }, {
-//   "id": 38575,
-//   "video": false,
-//   "vote_count": 2880,
-//   "vote_average": 6.3,
-//   "title": "The Karate Kid",
-//   "release_date": "2010-06-10",
-//   "original_language": "en",
-//   "original_title": "The Karate Kid",
-//   "genre_ids": [28, 12, 18, 10751],
-//   "backdrop_path": "/rfhnEYubWD4hnUUm8xXmotpqffb.jpg",
-//   "adult": false,
-//   "overview": "12-year-old Dre Parker could have been the most popular kid in Detroit, but his mother's latest career move has landed him in China. Dre immediately falls for his classmate Mei Ying but the cultural differences make such a friendship impossible. Even worse, Dre's feelings make him an enemy of the class bully, Cheng. With no friends in a strange land, Dre has nowhere to turn but maintenance man Mr. Han, who is a kung fu master. As Han teaches Dre that kung fu is not about punches and parries, but maturity and calm, Dre realizes that facing down the bullies will be the fight of his life.",
-//   "poster_path": "/bucxUHJd1DFy15iHzuXSOoFrObW.jpg",
-//   "popularity": 9.711,
-//   "media_type": "movie"
-// }]
+import {API_KEY as apikey, API_TOKEN as token} from "@env"
+
+
 export const trendingMovies = () => dispatch => {
   fetch(`https://api.themoviedb.org/3/trending/movie/day?api_key=${apikey}&token=${token}`)
     .then(res => res.json())
